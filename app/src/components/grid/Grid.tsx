c34153274438b49ff49f3ac3a4fa8bc7db2ddeeb
biv <biv@mail.com>
2024-12-03 18:40:16 +0100

    fix: change the grid responsive layout

--- app/src/components/grid/Grid.tsx
+++ app/src/components/grid/Grid.tsx
@@ -3,7 +3,7 @@ import { GridProps } from "@/@types/Grid";
 
 export function Grid({ cards }: GridProps){
     return(
-        <div className="columns-2 md:columns-3 lg:columns-5 gap-4 sm:px-0 px-4 my-5">
+        <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4 sm:px-0 px-4 my-5">
             {cards.map(card => (
                 <ImageCard key={card.id} {...card} />
             ))}
