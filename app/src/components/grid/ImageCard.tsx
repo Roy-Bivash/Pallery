865fee35ad864987ad68e5c6759a8d4ab1f7e73b
biv <biv@mail.com>
2024-12-05 11:27:07 +0100

    ui changes on the image drawer

--- app/src/components/grid/ImageCard.tsx
+++ app/src/components/grid/ImageCard.tsx
@@ -1,18 +1,12 @@
-import { ImageCardProps } from "@/@types/Grid";
-import { useDrawer } from "@/hooks/useDrawer";
-import { Button } from "@/components/ui/button"
-
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-  } from "@/components/ui/drawer"
 import { useState } from "react";
+
+import { ImageCardProps } from "@/@types/Grid";
+import { ArrowUpRight, Download, X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from "@/components/ui/drawer";
+
   
 export function ImageCard({ id, title, url}: ImageCardProps){
     const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
@@ -28,25 +22,37 @@ export function ImageCard({ id, title, url}: ImageCardProps){
                         loading="lazy"
                     />
                 </CardContent>
-                <CardFooter className="px-1 pt-2 pb-1">
-                    <p className="first-letter:capitalize truncate font-semibold">{title}</p>
-                </CardFooter>
+                {title.trim() && (
+                    <CardFooter className="px-1 pt-2 pb-1">
+                        <p className="first-letter:capitalize truncate font-semibold">{title}</p>
+                    </CardFooter>
+                )}
             </Card>
             <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
                 <DrawerContent>
-                    <DrawerHeader>
-                        <DrawerTitle>{title}</DrawerTitle>
-                        <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                    <div className="flex justify-end">
+                        <DrawerClose asChild>
+                            <X 
+                                height={24}
+                                width={24}
+                                className="cursor-pointer mx-4" 
+                                />
+                        </DrawerClose>
+                    </div>
+                    <DrawerHeader className="maxh--[25vh]">
+                        <DrawerTitle>{title || (<i>No title</i>)}</DrawerTitle>
+                        <DrawerDescription className="pt-2">
+                            {/* Description */}
+                            <span className="flex sm:flex-row flex-col gap-2">
+                                <Button variant="outline" className="w-fit">Open in a new tab <ArrowUpRight /></Button>
+                                <Button variant="ghost" className="w-fit">Dawnload <Download /></Button>
+                            </span>
+                        </DrawerDescription>
+
                     </DrawerHeader>
-                    <div className="w-fit">
+                    <div className="flex justify-center pb-8">
                         <img src={url} alt={title} className="max-h-[70vh]" />
                     </div>
-                    <DrawerFooter>
-                        <Button>Submit</Button>
-                        <DrawerClose asChild>
-                            <Button variant="outline">Cancel</Button>
-                        </DrawerClose>
-                    </DrawerFooter>
                 </DrawerContent>
             </Drawer>
         </>
