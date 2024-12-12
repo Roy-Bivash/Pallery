ec96a6b8af67aea38c675f7a319653c1be5ea638
biv <biv@mail.com>
2024-12-12 20:48:03 +0100

    search bar

--- app/src/components/sidebar/SidebarNav.tsx
+++ app/src/components/sidebar/SidebarNav.tsx
@@ -1,9 +1,34 @@
 import { DarkModeSwitch } from "../darkModeSwitch/DarkModeSwitch";
+import { Search } from "lucide-react"
+import { Label } from "@/components/ui/label"
+import { Input } from "../ui/input";
+import { useState } from "react";
+
 
 export function SidebarNav(){
+    const [searchInput, setSearchInput] = useState<string>("");
+
+    function SearchSubmit(e:React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        console.log(searchInput);
+        // TODO
+    }
 
     return(
         <div className="w-full flex items-center justify-end sm:px-4">
+            <form onSubmit={e => SearchSubmit(e)} className="relative w-full sm:w-fit mr-3 sm:mr-8">
+                <Label htmlFor="search" className="sr-only">
+                    Search
+                </Label>
+                <Input 
+                    id="search"
+                    name="search"
+                    onChange={e => setSearchInput(e.target.value)}
+                    placeholder="Search the images..."
+                    className="pl-9 rounded-full"
+                />
+                <Search className="pointer-events-none absolute left-3 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+            </form>
             <DarkModeSwitch />
         </div>
     )
